8417223f8e9a25d896163dbca872a74a7daacf83
Jermaine Acho Okere <127126885+jayokere@users.noreply.github.com>
2026-06-23 15:11:23 +0100

    feat(voice-activation-pipeline): add voice input pipeline orchestrating wake word and VAD

--- src/voice_concierge/voice_input/__init__.py
+++ src/voice_concierge/voice_input/__init__.py
@@ -1,7 +1,9 @@
 from voice_concierge.voice_input.voice_activity_detector import VoiceActivityDetector
+from voice_concierge.voice_input.voice_input_pipeline import VoiceInputPipeline
 from voice_concierge.voice_input.wake_word_detector import WakeWordDetector
 
 __all__ = [
     "WakeWordDetector",
     "VoiceActivityDetector",
+    "VoiceInputPipeline",
 ]
