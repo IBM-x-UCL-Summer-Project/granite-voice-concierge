12f6246cdb20ec79a383650104f3bd30b4364cdb
Jermaine Acho Okere <127126885+jayokere@users.noreply.github.com>
2026-07-13 10:58:26 +0100

    feat(command-control): add command event types

---
+++ src/voice_concierge/command_control/__init__.py
@@ -0,0 +1,8 @@
+"""Event-driven barge-in command control for playback interruption."""
+
+from voice_concierge.command_control.types import CommandEvent, PlaybackCommand
+
+__all__ = [
+    "CommandEvent",
+    "PlaybackCommand",
+]
